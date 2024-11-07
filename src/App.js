407db88c6f0da43e74f39dbb531b9941745c36d2
amansinghparihar708@gmail.com <amansinghparihar708@gmail.com>
2024-11-07 09:19:10 +0530

    upgrade code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Home from "./pages/Home";
 import DeliveryInformation from "./pages/DeliveryInformation";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Register from "./pages/account/account/Register";
+import Account from "./pages/account/account/Account";
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/ContactUs" element={<ContactUs />} />
           <Route path="/DeliveryInformation" element={<DeliveryInformation />} />
+          <Route path ="/pages/account/account/Account" element={<Account/>} />
         </Routes>
       </main>
 
