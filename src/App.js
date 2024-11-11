6f80f85e155dda0a07f950c33f8bed1677fffb8b
Prinssingh <Prinssingh7448@gmail.com>
2024-11-11 10:11:59 +0530

    Aman Merge

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from "./pages/account/account/Login";
 import Account from "./pages/account/account/Account";
 import Logout from "./pages/account/account/Logout";
 import Register from "./pages/account/account/Register";
-// import ShoppingCart from "./pages/account/ShoppingCart"
+import ShoppingCart from "./pages/account/ShoppingCart"
 import BlogList from "./pages/blog/BlogList";
 // import BlogDetails from "./pages/blog/BlogDetails";
 
@@ -31,9 +31,8 @@ function App() {
           <Route path ="/pages/account/account/Logout" element={<Logout/>} />
           <Route path ="/pages/account/account/Login" element={<Login/>} />
           <Route path ="/pages/account/account/Register" element={<Register/>} />
-          {/* <Route path ="/pages/account/ShoppingCart" element={<ShoppingCart/>} /> */}
+          <Route path ="/pages/account/ShoppingCart" element={<ShoppingCart/>} /> 
           <Route path="/BlogList" element={<BlogList />} />
-
           <Route path="/DeliveryInformation" element={<DeliveryInformation />} />
           
 
